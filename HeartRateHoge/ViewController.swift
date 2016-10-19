26c0ffd13f70cfeb37c2fd1d26914ae580d14232
Yuta Kase <yuta.kase.6@gmail.com>
2016-10-19 11:41:57 +0900

    Initial Commit

---
+++ HeartRateHoge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeartRateHoge
+//
+//  Created by Yuta Kase on 2016/10/19.
+//  Copyright © 2016年 Yuta Kase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
